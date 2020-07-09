16a076dfa1807c0b845826b49519e5d702e8091d
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-09 11:56:40 +0530

    Add files via upload

---
+++ gc.py
@@ -0,0 +1,6 @@
+import sys
+import os
+import getpass
+text_file = open("rc.conf", "w")
+text_file.write("[GC]\ntype = drive\nscope = drive\nservice_account_file = accounts/1.json\nservice_account_file_path = accounts/")
+text_file.close()
