d9e57c026a6191d4d6199f1bf5e35324845f3ae8
Roshan Connor <56798243+roshanconnor123@users.noreply.github.com>
2020-09-16 23:31:56 +0530

    Update gc.py

--- gc.py
+++ gc.py
@@ -2,5 +2,5 @@ import sys
 import os
 import getpass
 text_file = open("rclone.conf", "w")
-text_file.write("[GC]\ntype = drive\nscope = drive\nservice_account_file = accounts/1.json\nservice_account_file_path = accounts/")
+text_file.write("[GC]\ntype = drive\nscope = drive\nservice_account_file = /content/accounts/1.json\nservice_account_file_path = /content/accounts/")
 text_file.close()
